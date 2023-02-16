ccf1bc7ebe6cc331c3bbb11034f637dadb7f243a
myusername <myemail.com>
2023-02-16 11:43:40 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main-A program that prints a string to stdout
+ * main-A c program that prints a line using the printf function
  * return: 0 if success
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-
 return (0);
 }
