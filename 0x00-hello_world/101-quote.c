d3df1b668e5f49d4cabeefd6e40d39aabbf091a6
myusername <myemail.com>
2023-02-16 11:02:33 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,9 @@
  * main-A c program that prints a line to the standard error
  * return 1(SUccess)
  */
+
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
