e7eb992bca5f83e92ce8d9e483141560ccbc9274
myusername <myemail.com>
2023-02-21 15:33:01 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Check if char is lowercase
- * @C: is the char to be checked
+ * @c: is the char to be checked
  * Return: 1 if char is lowercase, otherwise 0.
  */
 
@@ -10,7 +10,7 @@ int _islower(int c)
 {
 	char c;
 
-	if (c >= 'a'; && c <= 'z';)
+	if (c >= 'a' && c <= 'z')
 	return (1);
 	else
 	return (0);
