16b300baeaa21d43d0d861056b448cf8954e7286
myusername <myemail.com>
2023-03-13 17:07:19 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: COunt arguments
+ * @argv: Arguments
+ * Return: Always 0(Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	/*Declaring variables*/
+	int count = 0;
+
+	if (argc > 0)
+	{
+	/*WHILE - Print each arguments*/
+	while (count < argc)
+	{
+	printf("%s\n", argv[count]);
+	count++;
+	}
+	}
+	return (0);
+}
