0fed8a84e2f698fb3d432b38644510219a95b354
myusername <myemail.com>
2023-03-20 14:51:52 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,12 +2,11 @@
 
 /**
  * main - prints the name of the file
- * _FILE_: contains the file name
  * Return: Always 0(Success)
  */
 
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
